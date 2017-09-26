39dfedf97e65d257c91bb230400e8268b7c0cafc
Grad Vasile <grad_simplu@yahoo.com>
2017-09-26 18:41:00 +0200

    CRUD and RESTful Routes

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,5 @@ Route::get('contact', 'PagesController@getContact');
 Route::get('about', 'PagesController@getAbout');
 
 Route::get('/', 'PagesController@getIndex');
+
+Route::resource('posts', 'PostController');
