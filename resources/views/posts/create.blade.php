118bfddaf6eda8d6c4e1c97fc5747cd8d4ea8960
Grad Vasile <grad_simplu@yahoo.com>
2017-09-26 22:13:38 +0200

    Javascript Form Validation using Parsley library

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,6 +2,12 @@
 
 @section('title', '| Create New Post')
 
+@section('stylesheets')
+
+	{!! Html::style('css/parsley.css') !!}
+
+@endsection
+
 @section('content')
 	
 	<div class="row">
@@ -9,17 +15,23 @@
 			<h1>Create New Post</h1>
 			<hr>
 
-			{!! Form::open(['route' => 'posts.store']) !!}
+			{!! Form::open(['route' => 'posts.store', 'data-parsley-validate' => '']) !!}
 			    {{ Form::label('title', 'Title:') }}
-			    {{ Form::text('title', null, array('class' => 'form-control')) }}
+			    {{ Form::text('title', null, array('class' => 'form-control', 'required' => '', 'maxlength' => '255')) }}
 
 			    {{ Form::label('body', 'Post Body:')}}
-			    {{ Form::textarea('body', null, array('class' => 'form-control')) }}
+			    {{ Form::textarea('body', null, array('class' => 'form-control', 'required' => '')) }}
 
 			    {{ Form::submit('Create Post', array('class' => 'btn btn-success btn-lg btn-block', 'style' => 'margin-top: 20px;')) }}
 			{!! Form::close() !!}
 
 		</div>
 	</div>
+
+@endsection
+
+@section('scripts')
+
+	{!! Html::script('js/parsley.min.js') !!}
 
 @endsection
