0462487b438eea32c9c47cc118484e8d773f232c
Grad Vasile <grad_simplu@yahoo.com>
2017-09-27 21:57:54 +0200

    Read our CRUD - Create and show all the Posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -18,7 +18,12 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        // create a variable and store all the blog posts from DB
+        $posts = Post::all();
+
+        // return a view and pass in the above variable
+        return view('posts.index')->withPosts($posts);
+
     }
 
     /**
