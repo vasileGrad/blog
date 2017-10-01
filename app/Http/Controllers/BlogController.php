9563207b08c6921c483391d2f3c0c70d2e16f9a6
Grad Vasile <grad_simplu@yahoo.com>
2017-10-01 16:03:17 +0200

    Adding Freatures to our Blog Controller. Add Blog in the navbar

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -8,6 +8,12 @@ use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
+	// action = a method inside a Controller
+	public function getIndex() {
+		$posts = Post::paginate(2);
+		return view('blog.index')->withPosts($posts);
+	}
+
     public function getSingle($slug) {
     	// Fetch from the DB based on slug
     	$post = Post::where('slug', '=', $slug)->first();
