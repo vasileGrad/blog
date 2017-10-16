a4e2a8fd3cdc0d4be303b6121aeb335b2bdb9127
Grad Vasile <grad_simplu@yahoo.com>
2017-10-16 19:11:11 +0300

    Show, Edit, Update our Tags

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -50,7 +50,8 @@ class TagController extends Controller
      */
     public function show($id)
     {
-        //
+        $tag = Tag::find($id);
+        return view('tags.show')->withTag($tag);
     }
 
     /**
@@ -61,7 +62,8 @@ class TagController extends Controller
      */
     public function edit($id)
     {
-        //
+        $tag = Tag::find($id);
+        return view('tags.edit')->withTag($tag);
     }
 
     /**
@@ -73,7 +75,16 @@ class TagController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $tag = Tag::find($id);
+
+        $this->validate($request, ['name' => 'required|max:255']);
+
+        $tag->name = $request->name;
+        $tag->save();
+
+        Session::flash('success', 'Successfully saved you new tag!');
+
+        return redirect()->route('tags.show', $tag->id);
     }
 
     /**
---
+++ resources/views/tags/edit.blade.php
@@ -0,0 +1,19 @@
+@extends('main')
+
+@section('title', "| Edit Tag")
+
+@section('content')
+
+	<!-- Form::model() does the model form binding -->
+	<!--  Takes that model that we have, It's gonna pass in here and it's gonna fill it in-->
+	<!-- $tag - this is the model that we want to fill into the Form -->
+	{{ Form::model($tag, ['route' => ['tags.update', $tag->id], 'method' => "PUT"]) }}
+
+		{{ Form::label('name', "Title:") }}
+		{{ Form::text('name', null, ['class' => 'form-control']) }}
+
+		{{ Form::submit('Save Changes', ['class' => 'btn btn-success margin-top-button']) }}
+
+	{{ Form::close() }}
+
+@endsection
