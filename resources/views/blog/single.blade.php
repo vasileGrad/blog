59180274ee2ba18f1b7244711b16d68ada2519e9
Grad Vasile <grad_simplu@yahoo.com>
2017-10-19 20:06:55 +0300

    Styling Comments and adding gravatar images

--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -15,10 +15,22 @@
 
 	<div class="row">
 		<div class="col-md-8 col-md-offset-2">
+			<h3 class="comments-title"><span class="glyphicon glyphicon-comment"></span>{{ $post->comments()->count() }} Comments</h3>
 			@foreach($post->comments as $comment)
 				<div class="comment">
-					<p><strong>Name:</strong> {{ $comment->name }}</p>
-					<p><strong>Comment:</strong> {{ $comment->comment }}</p><br>
+					<div class="author-info">
+						<img src="{{ "https://www.gravatar.com/avatar/" . md5(strtolower(trim($comment->email))) . "?s=50&d=identicon" }}" class="author-image">
+						<div class="author-name">
+							<h4>{{ $comment->name }}</h4>
+							<p class="author-time">{{ date('F jS, Y - g:i A', strtotime($comment->created_at)) }}</p>
+						</div>
+						
+					</div>
+
+					<div class="comment-content">
+						{{ $comment->comment }}
+					</div>
+					
 				</div>
 			@endforeach
 		</div>
