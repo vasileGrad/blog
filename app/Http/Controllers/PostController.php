b8e5605691b43a608be7fa44f8a42e323707825b
Grad Vasile <grad_simplu@yahoo.com>
2017-10-21 19:55:16 +0300

    Security Concerns with WYSIWYG Editors using Purifier

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,6 +10,7 @@ use App\Post;
 use App\Tag;
 use App\Category;
 use Session;
+use Purifier;
 
 class PostController extends Controller
 {
@@ -81,7 +82,9 @@ class PostController extends Controller
         $post->title = $request->title;
         $post->slug = $request->slug;
         $post->category_id = $request->category_id;
-        $post->body = $request->body;
+
+        // we use Purifier to clean and secure
+        $post->body = Purifier::clean($request->body);
 
         $post->save(); // save the object
         // save the new item into the Database
@@ -191,7 +194,7 @@ class PostController extends Controller
         $post->title = $request->input('title');
         $post->slug = $request->input('slug');
         $post->category_id = $request->input('category_id');
-        $post->body = $request->input('body');
+        $post->body = Purifier::clean($request->input('body'));
 
         $post->save();
 
